2a07e3c123f6c69e324bd71304ae7d62d202f6c1
Gopesh Sharma <gopesh.sharma@gmail.com>
2022-05-26 17:30:10 -0400

    sample types

--- typescript-training/02-sampletypes/sample-types.js
+++ typescript-training/02-sampletypes/sample-types.js
@@ -1,10 +1,12 @@
 //defining variables
-var found = true;
-var age = 54;
-var firstName = "Gopesh";
-var lastName = "Sharma";
-console.log(found);
-console.log(age);
-console.log(firstName);
-console.log(lastName);
-console.log("Hi! My name is ".concat(firstName, " ").concat(lastName));
+if (true) {
+    var found = true;
+    var age = 54;
+    var firstName = "Gopesh";
+    var lastName = "Sharma";
+    console.log(found);
+    console.log(age);
+    console.log(firstName);
+    console.log(lastName);
+    console.log("Hi! My name is ".concat(firstName, " ").concat(lastName));
+}
--- typescript-training/02-sampletypes/sample-types.ts
+++ typescript-training/02-sampletypes/sample-types.ts
@@ -1,14 +1,13 @@
 //defining variables
-
-let found: boolean = true;
-let age: number = 54;
-let firstName: string = "Gopesh";
-let lastName: string = "Sharma";
-
-
-console.log(found);
-console.log(age);
-console.log(firstName);
-console.log(lastName);
-
-console.log(`Hi! My name is ${firstName} ${lastName}`);
+if (true) {
+    let found: boolean = true;
+    let age: number = 54;
+    let firstName: string = "Gopesh";
+    let lastName: string = "Sharma";
+    
+    console.log(found);
+    console.log(age);
+    console.log(firstName);
+    console.log(lastName);
+    console.log(`Hi! My name is ${firstName} ${lastName}`);
+}
