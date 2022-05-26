2837d0b793f2cf25ce0d1503d938badcc43064b5
Gopesh Sharma <gopesh.sharma@gmail.com>
2022-05-26 07:36:13 -0400

    some type script basics

---
+++ typescript-training/02-sampletypes/sample-types.ts
@@ -0,0 +1,14 @@
+//defining variables
+
+let found: boolean = true;
+let age: number = 54;
+let firstName: string = "Gopesh";
+let lastName: string = "Sharma";
+
+
+console.log(found);
+console.log(age);
+console.log(firstName);
+console.log(lastName);
+
+console.log(`Hi! My name is ${firstName} ${lastName}`);
